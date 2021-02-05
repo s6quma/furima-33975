9246f13664272a41b66cb596f11bc002ec938857
s6quma <s6quma@yahoo.co.jp>
2021-02-05 11:06:03 +0900

    バリデーションの記述変更
    重複している記述をwith_optionsでまとめた

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
+  
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
-  validates :first_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
-  validates :last_name,        presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
-  validates :first_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
-  validates :last_name_kana,   presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
-  validates :birthday,         presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+  end
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' } do
+    validates :first_name
+    validates :last_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
 end
