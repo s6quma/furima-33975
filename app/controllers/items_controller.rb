0a578a78801f0148279fc6a8741a341ed3252bd3
s6quma <s6quma@yahoo.co.jp>
2021-02-06 09:57:31 +0900

    商品をDBに保存する設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,23 @@
 class ItemsController < ApplicationController
   def index
+    @item =Item.new
+  end
+
+  def new
+     @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path(@item)
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:title, :description, :price, :category_id, :condition_id, :shipping_charge_id, :area_id, :shipping_date_id).merge(user_id: current_user.id)
   end
 end
