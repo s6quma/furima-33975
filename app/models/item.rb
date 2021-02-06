fec3cdc0e8e87746f35998af7fad76656c354feb
s6quma <s6quma@yahoo.co.jp>
2021-02-06 09:58:28 +0900

    active_hashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
+
+  validates :category_id, :condition_id, :shipping_charge_id, :area_id, :shipping_date_id, numericality: { other_than: 1 }
 end
