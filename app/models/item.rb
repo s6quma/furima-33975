2e5474ea8734516d9c8aa68286647bfb90d122ea
s6quma <s6quma@yahoo.co.jp>
2021-02-07 18:39:50 +0900

    モデル単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,14 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one_attached :image
-
-  validates :category_id, :condition_id, :shipping_charge_id, :area_id, :shipping_date_id, numericality: { other_than: 1 }
+  
+  with_options presence: true do
+    validates :image
+    validates :title
+    validates :description
+    validates :price
+  end
+  validates :category_id, :condition_id, :shipping_charge_id, :area_id, :shipping_date_id, numericality: { other_than: 1}
+  validates :price, numericality: { only_integer: true, message: "is valid. Input half-width number" }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range" }
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,4 +1,16 @@
 FactoryBot.define do
   factory :item do
+  title {'test'}
+  description {Faker::Lorem.sentence}
+  price {Faker::Number.within(range: 300..9999999)}
+  category_id {'5'}
+  condition_id {'2'}
+  shipping_charge_id {'2'}
+  area_id {'40'}
+  shipping_date_id {'2'}
+  association :user
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
