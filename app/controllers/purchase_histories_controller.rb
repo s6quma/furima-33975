d0cc34b2671a6084cbbd78e16f946cbb91847af3
s6quma <s6quma@yahoo.co.jp>
2021-02-10 15:25:38 +0900

    データベースのテーブル作成

--- app/controllers/purchase_histories_controller.rb
+++ app/controllers/purchase_histories_controller.rb
@@ -1,7 +1,11 @@
 class PurchaseHistoriesController < ApplicationController
+  before_action :authenticate_user!
   def index
   end
 
   def new
+  end
+
+  def create
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items do
-   resources :purchase_histories, only: [:index, :new]
+   resources :purchase_histories, only: [:index, :new, :create]
   end
 end
