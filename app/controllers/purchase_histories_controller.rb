c79d06efe1b354139b8c97fba4c79f56e39c6506
s6quma <s6quma@yahoo.co.jp>
2021-02-11 13:25:24 +0900

    dbに商品購入情報を保存する実装

--- app/controllers/purchase_histories_controller.rb
+++ app/controllers/purchase_histories_controller.rb
@@ -1,11 +1,23 @@
 class PurchaseHistoriesController < ApplicationController
   before_action :authenticate_user!
   def index
-  end
-
-  def new
+    @purchase_history_address = PurchaseHistoryAddress.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @purchase_history_address = PurchaseHistoryAddress.new(purchase_history_address_params)
+    @item = Item.find(params[:item_id])
+    if @purchase_history_address.valid?
+      @purchase_history_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def purchase_history_address_params
+    params.require(:purchase_history_address).permit(:postal_code, :prefecture, :city, :block_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
