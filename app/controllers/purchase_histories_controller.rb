4b04e5591a185a469e4c57447245598c665b9133
s6quma <s6quma@yahoo.co.jp>
2021-02-13 13:02:20 +0900

    before_actionへset_purchase_historyを定義

--- app/controllers/purchase_histories_controller.rb
+++ app/controllers/purchase_histories_controller.rb
@@ -1,15 +1,14 @@
 class PurchaseHistoriesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_purchase_history, only: [:index, :create, :move_to_index]
   before_action :move_to_index
 
   def index
     @purchase_history_address = PurchaseHistoryAddress.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
     @purchase_history_address = PurchaseHistoryAddress.new(purchase_history_address_params)
-    @item = Item.find(params[:item_id])
     if @purchase_history_address.valid?
       pay_item
       @purchase_history_address.save
@@ -27,6 +26,10 @@ class PurchaseHistoriesController < ApplicationController
     )
   end
 
+  def set_purchase_history
+    @item = Item.find(params[:item_id])
+  end
+
   def pay_item
     Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
@@ -37,7 +40,6 @@ class PurchaseHistoriesController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:item_id])
     redirect_to root_path if @item.purchase_history != nil || @item.user_id == current_user.id
   end
 end
